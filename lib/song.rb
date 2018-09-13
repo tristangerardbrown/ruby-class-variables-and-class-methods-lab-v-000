9840b5d3c5f4ed0ec7a19d0038807ef0f33df641
Tristan Brown <tgbrown@stanford.edu>
2018-09-13 03:42:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,17 @@ class Song
     @@count += 1
   end
 
-  
+  def self.count
+    @@count
+  end
+
+  def self.genres
+     @@genres.uniq
+   end
+
+  def self.artists
+     @@artists.uniq
+   end
 
     def self.genre_count
    genre_count = {}
